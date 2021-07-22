bc3bf94259c5832854f2e813b58c04765a4e7e93
mmilton <murray.l.milton@outlook.com>
2021-07-22 20:42:34 +0200

    added an iframe video for testing with API key and page arrangement

--- src/Components/VideoSearch/videoSearch.jsx
+++ src/Components/VideoSearch/videoSearch.jsx
@@ -1,3 +1,12 @@
 import React, {Component} from 'react';
 
+class VideoSearch extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            search:" "
+        }
+    }
+}
+
 export default VideoSearch;
--- src/Components/app.jsx
+++ src/Components/app.jsx
@@ -1,13 +1,14 @@
 import { render } from '@testing-library/react';
 import React, {Component} from'react';
-
+import VideoDisplay from './VideoDisplay/videoDisplay';
 
 
 class App extends Component{
 render(){
     return(
         <React.Fragment>
-            <h1>Hello</h1>
+            <h1>All about cats</h1>
+            <VideoDisplay/>
         </React.Fragment>
     )
   }
