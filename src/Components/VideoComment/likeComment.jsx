f5fee53c9a4a510c22f6ad4f2baf1795028651b2
mmilton <murray.l.milton@outlook.com>
2021-07-22 19:52:31 +0200

    Made corrections to wireframe model

--- src/Components/VideoComment/likeComment.jsx
+++ src/Components/VideoComment/likeComment.jsx
@@ -1,3 +1,24 @@
+import { render } from '@testing-library/react';
 import React, {Component} from 'react';
+
+class LikeComment extends Component{
+
+// Set for Friday
+    constructor(props);{
+        super(props);
+        this.state = {
+            Comment:[]
+        }
+    }
+    render() {
+        return(
+            <React.Fragment>
+                <div></div>
+            </React.Fragment>
+        )
+        
+    }
+}
+
 
 export default LikeComment;
