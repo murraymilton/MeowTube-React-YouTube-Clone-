d1b4c39f150bf1314c637641b2cd473784b2391a
mmilton <murray.l.milton@outlook.com>
2021-07-22 17:27:18 +0200

    Added wireframe

---
+++ src/Components/app.jsx
@@ -0,0 +1,15 @@
+import { render } from '@testing-library/react';
+import React, {Component} from'react';
+
+
+
+class App extends Component{
+render(){
+    return(
+        <React.Fragment>
+            <h1>Hello</h1>
+        </React.Fragment>
+    )
+  }
+}
+export default App;
