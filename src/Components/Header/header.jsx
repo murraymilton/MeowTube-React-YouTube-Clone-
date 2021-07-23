aaf4beeebf06ea39cff99d8501d6f39d7dd79afb
mmilton <murray.l.milton@outlook.com>
2021-07-23 22:37:00 +0200

    Reloved merge conflicts

--- src/Components/Header/header.jsx
+++ src/Components/Header/header.jsx
@@ -4,11 +4,7 @@ import './_header.scss';
 const Header = () => {
     return(
         <div>
-<<<<<<< HEAD
-            
-=======
-            MEOW TUBE-------Placeholder for HEADER
->>>>>>> 148e158cb114fe0d72c68e70e85412a89cda793a
+    
         </div>
     )
 }
