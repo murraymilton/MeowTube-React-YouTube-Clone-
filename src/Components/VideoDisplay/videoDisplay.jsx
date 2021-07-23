e4a043ae00fc018d6fbfabbe7f32964bc33e9bd6
mmilton <murray.l.milton@outlook.com>
2021-07-23 18:17:53 +0200

    merge conflict resloved

--- src/Components/VideoDisplay/videoDisplay.jsx
+++ src/Components/VideoDisplay/videoDisplay.jsx
@@ -8,11 +8,17 @@ class VideoDisplay extends Component {
     constructor(props){
     super(props);
     this.state = {
-        video_id: "",
+        comment:[video_id: "",
         author:"",
         text:"",
         likes: 0,
         videoId:''
+    ],
+    videos: [
+        {
+            videoId:''
+        }
+    ]
         }
 
     } //{this.state.video_id}
@@ -20,7 +26,9 @@ class VideoDisplay extends Component {
 render(){
     return(
         <React.Fragment>
-           <div className="responsive-video"> <iframe id="ytplayer" type="text/html" width="640" height="360"
+           <div className="responsive-video"> 
+           <h2>{this.props.video}</h2>
+           <iframe id="ytplayer" type="text/html" width="640" height="360"
   src="https://www.youtube.com/embed/hY7m5jjJ9mM?autoplay=1&origin=http://example.com"
   frameBorder="0"></iframe></div>
         </React.Fragment>
