3711d9341ca3145999bf822deae1a74abbef67c8
mmilton <murray.l.milton@outlook.com>
2021-07-28 15:01:04 +0200

    made revisions to recommended video section for thumbnail

--- src/Components/RecommendedVideos/recommendedVideos.jsx
+++ src/Components/RecommendedVideos/recommendedVideos.jsx
@@ -12,20 +12,22 @@ function RecommendedVideos () {
                 views="10.7M Views"
                 timestamp="3 days"
                 channelImage="https://i.ytimg.com/vi/8FUOnGMc66w/maxresdefault.jpg"
-                channel="CatsInTheMiddle"/>
-                <ThumbnailDisplay />
-                <ThumbnailDisplay />
-                <ThumbnailDisplay />
-                <ThumbnailDisplay />
-                <ThumbnailDisplay />
-                <ThumbnailDisplay />
-                <ThumbnailDisplay />
-                <ThumbnailDisplay />
-                <ThumbnailDisplay />
-                <ThumbnailDisplay />
-                <ThumbnailDisplay />
-                <ThumbnailDisplay />
-                <ThumbnailDisplay />
+                channel="CatsInTheMiddle"
+                image=""/>
+                <ThumbnailDisplay 
+                title="Cats Dance"
+                views="10.7M Views"
+                timestamp="3 days"
+                channelImage="https://i.ytimg.com/vi/8FUOnGMc66w/maxresdefault.jpg"
+                channel="CatsInTheMiddle"
+                image=""/>
+                <ThumbnailDisplay 
+                title="Cats Dance"
+                views="10.7M Views"
+                timestamp="3 days"
+                channelImage="https://i.ytimg.com/vi/8FUOnGMc66w/maxresdefault.jpg"
+                channel="CatsInTheMiddle"
+                image=""/>
             </div>
         </div>
     )
