40476aca534e94aad14d3290c808aad982efe16c
mmilton <murray.l.milton@outlook.com>
2021-07-28 12:59:45 +0200

    Added property components and added styling

---
+++ src/Components/RecommendedVideos/recommendedVideos.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './recommendedVideos.css'
+
+function RecommendedVideos () {
+    return (
+        <div className="recommendedVideos">
+            <h2>Recommendations</h2>
+        </div>
+    )
+}
+
+export default RecommendedVideos;
--- src/Components/SideBar/sideBar.jsx
+++ src/Components/SideBar/sideBar.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
-import './_sidebar.scss';
-const Sidebar = () => {
-    return(
-        <div>
-            Side Bar for related videos
+import {MdHistory, MdHome,  MdPlaylistPlay, MdRssFeed, MdVideoLibrary, MdWatchLater} from 'react-icons/md'
+import { FaFutbol, FaGripfire, FaMusic, FaNewspaper } from 'react-icons/fa';
+import {BsFillCollectionPlayFill} from 'react-icons/bs';
+import {GiGamepad} from 'react-icons/gi';
+import './sideBar.css';
+import SideBarRow from './SideBarRow';
+
+
+function SideBar () {
+    return (
+        <div className="sidebar">
+            <SideBarRow Icon={MdHome}title="Home" color="red" />
+            <SideBarRow Icon={FaGripfire}title="Trending"/>
+            <SideBarRow Icon={BsFillCollectionPlayFill}title="Subsriptions"/>
+            <SideBarRow Icon={MdVideoLibrary}title="Video"/>
+            <SideBarRow Icon={MdHome}title="Home"/>
+            <SideBarRow Icon={MdHome}title="Home"/>
+            <SideBarRow Icon={MdHome}title="Home"/>
+            <SideBarRow Icon={MdHome}title="Home"/>
+            <SideBarRow Icon={MdHome}title="Home"/>
+            <SideBarRow Icon={MdHome}title="Home"/>
+            <SideBarRow Icon={MdHome}title="Home"/>
+
         </div>
     )
 }
 
-export default Sidebar;
+export default SideBar;
