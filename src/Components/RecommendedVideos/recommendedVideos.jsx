0a384e25f08c4600c43842d8636a4967b67f0e8f
mmilton <murray.l.milton@outlook.com>
2021-07-28 14:46:09 +0200

    made revisions to thumbnails

--- src/Components/RecommendedVideos/recommendedVideos.jsx
+++ src/Components/RecommendedVideos/recommendedVideos.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
 import './recommendedVideos.css'
+import ThumbnailDisplay from '../ThumbnailDisplay/thumbnailDisplay';
 
 function RecommendedVideos () {
     return (
         <div className="recommendedVideos">
             <h2>Recommended</h2>
+            <div className="recommendedVideos__videos">
+                <ThumbnailDisplay 
+                title="Cats Dance"
+                views="10.7M Views"
+                timestamp="3 days"
+                channelImage="https://i.ytimg.com/vi/8FUOnGMc66w/maxresdefault.jpg"
+                channel="CatsInTheMiddle"/>
+                <ThumbnailDisplay />
+                <ThumbnailDisplay />
+                <ThumbnailDisplay />
+                <ThumbnailDisplay />
+                <ThumbnailDisplay />
+                <ThumbnailDisplay />
+                <ThumbnailDisplay />
+                <ThumbnailDisplay />
+                <ThumbnailDisplay />
+                <ThumbnailDisplay />
+                <ThumbnailDisplay />
+                <ThumbnailDisplay />
+                <ThumbnailDisplay />
+            </div>
         </div>
     )
 }
